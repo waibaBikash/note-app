0a2b90ad7273e405d09d6f2649a90ed3457e89c0
waibaBikash <kavrelibikash@gmail.com>
2024-05-29 14:07:36 +1000

    store a note on local storage

--- file.js
+++ file.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+ function showNotes(){
+   notesContainer.innerHTML = localStorage.getItem("notes");
+ }
+ showNotes();
+
+function updateStorage(){
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", ()=>{
   let inputBox = document.createElement("p");
   let img = document.createElement("img");
@@ -13,5 +22,14 @@ createBtn.addEventListener("click", ()=>{
 notesContainer.addEventListener("click", function(e){
   if(e.target.tagName === "IMG"){
      e.target.parentElement.remove();
+     updateStorage();
+  }
+  else if(e.target.tagName === "p"){
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function (){
+        updateStorage();
+      }
+    })
   }
 })
